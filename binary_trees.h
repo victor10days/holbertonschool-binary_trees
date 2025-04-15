63019654f270a57c9045a75bb3795bb724e32ebe
victor10days <victor10days@gmail.com>
2025-04-15 17:26:01 -0400

    binary trees

---
+++ binary_trees.h
@@ -0,0 +1,20 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
